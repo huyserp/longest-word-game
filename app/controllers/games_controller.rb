980ed89cb322722b62cfc8838c8f83be10d25c12
peter huyser <huyserp@gmail.com>
2021-01-08 16:59:03 +0100

    working app, just require style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(10) { ('A'..'Z').to_a.sample }
@@ -6,7 +9,8 @@ class GamesController < ApplicationController
   def score
     @guess = params[:word].upcase
     @letters = params[:letters]
-    valid_with_grid = word_in_grid?(@guess, @letters) ? "valid" : "not valid"
+    @valid_with_grid = word_in_grid?(@guess, @letters)
+    @valid_english = check_dictionary(@guess)
   end
 
   private
@@ -15,19 +19,11 @@ class GamesController < ApplicationController
     guess.chars.all? { |letter| guess.count(letter) <= letters.count(letter) }
   end
 
-  def check_dictionary
+  def check_dictionary(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word.downcase}"
+    dictionary = JSON.parse(open(url).read)
+    dictionary['found']
   end
 
-  def check_results
-  end
+  # def check_results; end
 end
-# def score
-#     @guess = params[:word]
-#     @letters = params[:letters].downcase
-#     if @guess.chars.all? { |letter| @guess.count(letter) <= @letters.count(letter) }
-#       @score = "Well done!"
-#     else
-#       @score = "Try again!"
-#     end
-#   end
-# end
